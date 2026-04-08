06d7b36ab56d2972c5f149cda81ffb8e294222f7
abenezer-lab-code <abenezerforcode@gmail.com>
2026-04-08 19:45:27 +0300

    adding project

---
+++ script.js
@@ -0,0 +1,39 @@
+// select buttons and apply show hide using toogel.
+// apply for keyboard users
+// update button states for screen readeres
+const btns = document.querySelectorAll(".question-btn");
+const answers = document.querySelectorAll(".answer");
+const btnsImages  = document.querySelectorAll(".question-btn img");
+
+btns.forEach(btn=>{
+btn.addEventListener("click",()=>{
+   const idName = btn.getAttribute("aria-controls");
+   const elemntToShow = document.getElementById(idName);
+  const activeBtnImg = document.querySelector(`button[aria-controls="${idName}"] img`);
+answers.forEach(answer=>{
+if(answer.id !==idName){
+ answer.classList.remove("display")  
+}
+
+})
+btnsImages.forEach(img=>{
+   img.src="assets/images/icon-plus.svg"
+})
+btns.forEach(btn=>{
+   btn.setAttribute("aria-expanded","false")
+})
+
+elemntToShow.classList.toggle("display") 
+if(elemntToShow.classList.contains("display")){
+ btn.setAttribute("aria-expanded",true)
+activeBtnImg.src="assets/images/icon-minus.svg"
+}else{
+btn.setAttribute("aria-expanded",false)
+   activeBtnImg.src="assets/images/icon-plus.svg"
+
+}
+
+})
+
+})
+
